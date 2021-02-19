d2bc17d95d09780a014edccaf42ea5840f998a50
donaldcrane <donaldagbakoba@gmail.com>
2021-02-19 23:49:18 +0100

    modified card.php

--- card.php
+++ card.php
@@ -1,6 +1,6 @@
 <?php
     for($i = 0; $i < 201; $i++){
-        $number = mt_rand(100000000000, 945234679086). "<br>";
-        echo $number;
+        $number = mt_rand(100000000000, 945234679086);
+        echo $number. "<br>";
     } 
 ?>
